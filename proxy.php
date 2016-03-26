4566e701789c387bb20728170b22392fa1cafe2a
Alex Bieg <Alex Bieg>
2016-03-26 12:29:25 -0700

    asdf

--- proxy.php
+++ proxy.php
@@ -1,8 +1,6 @@
 <?php
     header('Content-Type: text/plain');
 
-    $params = json_decode(file_get_contents('php://input'), true);
-
     if ( isset($_POST['address'])) {
         echo file_get_contents($params['address']);
     } else {
